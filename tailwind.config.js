b090161a6f56faa0bc660e7a0b19d0ee432522b4
Aadarsh1010 <work.aadarsh11@gmail.com>
2026-05-10 03:14:57 +0545

    first2

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,21 +5,7 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        display: ['Playfair Display', 'serif'],
-        sans: ['Inter', 'system-ui', 'sans-serif'],
-      },
-      colors: {
-        primary: {
-          DEFAULT: '#2C1810',
-          light: '#5C2A2A',
-        },
-        gold: '#D4AF37',
-        saffron: '#E67E22',
-        cream: '#F8F1E3',
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
